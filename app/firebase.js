af7b76d94272d70dbb60a09c2397795f66a94337
mahmoud1134 <mm81641456789644@gmail.com>
2025-05-08 15:49:25 -0700

    add cart page and update product page and uploda database

--- app/firebase.js
+++ app/firebase.js
@@ -1,20 +1,22 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDHe15a7Fq_Pl9lThsiUrXpGmoux5NliYM",
   authDomain: "project-test-35fb7.firebaseapp.com",
   databaseURL: "https://project-test-35fb7-default-rtdb.firebaseio.com",
   projectId: "project-test-35fb7",
-  storageBucket: "project-test-35fb7.appspot.com", 
+  storageBucket: "project-test-35fb7.firebasestorage.app",
   messagingSenderId: "765689248226",
   appId: "1:765689248226:web:d674c51e4425d455c761bb"
 };
 
-
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export default auth;
+const db = getFirestore(app);
+
+export { auth, db };
